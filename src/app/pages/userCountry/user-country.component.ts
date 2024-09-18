0e3b1308e16f64e3f65886768a93f233d2e04c4c
isita <isita@banregio.com>
2024-09-18 17:06:11 -0600

    Inicio de proyecto

---
+++ src/app/pages/userCountry/user-country.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-country',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-country.component.html',
+  styleUrl: './user-country.component.css'
+})
+export class UserCountryComponent {
+
+}
