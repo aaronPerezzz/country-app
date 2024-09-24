9b661224334b57253268e5a835e8075a45fcf3ed
aaron.perez <isita@banregio.com>
2024-09-24 09:36:15 -0600

    Se documenta codigo. *Se agrega metodo para cerrar sesion

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -19,12 +19,21 @@ export class AuthService {
   private toastService = inject(ToastService);
   private cookieService = inject(CookieService);
 
+  /**
+   * Lee archivo JSON de usuarios
+   * @returns Observable<User[]>
+   */
   public readUsersJSON(): Observable<User[]>{
     return this.httpClient
     .get<User[]>(Constants.JSON_USERS)
     .pipe(catchError(this.handleError<any>("readUserJSON", null)));
   }
 
+  /**
+   * Realiza peticion a API para crear token
+   * @param userInfo token
+   * @returns
+   */
   public loginUser(userInfo: User){
     return this.httpClient.post(`${Constants.URL_LOGIN}/api/login`, {
       email: userInfo.email,
@@ -32,6 +41,11 @@ export class AuthService {
     }).pipe(catchError(this.handleError<any>("loginUser", null)));
   }
 
+  /**
+   * Guarda token en cookies de navegador
+   * @param token
+   * @param user
+   */
   public setToken(token: string, user: User){
     this.cookieService.set(Constants.ID_TOKEN, JSON.stringify({
       token,
@@ -39,14 +53,35 @@ export class AuthService {
     }));
   }
 
+  /**
+   * Obtiene informacion guardada en cookies
+   * @returns JSON
+   */
   public getToken(){
     return JSON.parse(this.cookieService.get(Constants.ID_TOKEN));
   }
 
+  /**
+   * Verifica si existe información en cookies
+   * @returns boolean
+   */
   public existToken(){
     return this.cookieService.check(Constants.ID_TOKEN);
   }
 
+  /**
+   * Elimina información guardad en cookies
+   */
+  public logout(){
+    this.cookieService.delete(Constants.ID_TOKEN);
+  }
+
+  /**
+   * Captura error al realizar peticiones
+   * @param operation
+   * @param result
+   * @returns
+   */
 
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
